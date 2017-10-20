e729f4596213735a57e707cf1008d428dfd4b060
pampas93 <abhijit.93@hotmail.com>
2017-10-20 13:22:36 -0400

    Button A to change views + scene setup

--- Mindfulness_Oculus/Assets/SittingTerrain/ChangeViews.cs
+++ Mindfulness_Oculus/Assets/SittingTerrain/ChangeViews.cs
@@ -6,15 +6,24 @@ using VRTK;
 public class ChangeViews : MonoBehaviour
 {
 
-    public Transform SpawnPoint_1;
-    public GameObject VRTK_Camera;
+    //public Transform SpawnPoint_1;
 
+    public GameObject VRTK_Camera;
+    public GameObject Character;
+
+    public Transform[] spawnPoints;
+
+    private int spawnIndex = 0;
 
     void Start()
     {
 
-        GetComponent<VRTK_ControllerEvents>().ButtonTwoPressed += new ControllerInteractionEventHandler(ThirdPersonView);
+        VRTK_Camera.transform.position = spawnPoints[spawnIndex].position;
+        VRTK_Camera.transform.rotation = spawnPoints[spawnIndex].rotation;
+        Character.SetActive(false);
 
+
+        GetComponent<VRTK_ControllerEvents>().ButtonOnePressed += new ControllerInteractionEventHandler(ThirdPersonView);
         //spawn1.position = new Vector3(0.0f, 3.3f, 1.4f);
         
        
@@ -22,7 +31,18 @@ public class ChangeViews : MonoBehaviour
 
     private void ThirdPersonView(object sender, ControllerInteractionEventArgs e)
     {
-        VRTK_Camera.transform.position = SpawnPoint_1.position;
-        VRTK_Camera.transform.rotation = SpawnPoint_1.rotation;
+        spawnIndex++;
+        spawnIndex = spawnIndex % spawnPoints.Length;
+
+        VRTK_Camera.transform.position = spawnPoints[spawnIndex].position;
+        VRTK_Camera.transform.rotation = spawnPoints[spawnIndex].rotation;
+
+        if(spawnIndex == 0)
+        {
+            Character.SetActive(false);
+        }else
+        {
+            Character.SetActive(true);
+        }
     }
 }
