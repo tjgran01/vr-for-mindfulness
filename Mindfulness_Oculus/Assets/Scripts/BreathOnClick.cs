55107f0d284638e895da640e88d7257032771783
pampas93 <abhijit.93@hotmail.com>
2017-11-17 14:22:35 -0500

    Bug fix with Capture data from server

---
+++ Mindfulness_Oculus/Assets/Scripts/BreathOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BreathOnClick : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<VRTK_ControllerEvents>().ButtonOnePressed += new ControllerInteractionEventHandler(InhaleNow);
+        GetComponent<VRTK_ControllerEvents>().ButtonTwoPressed += new ControllerInteractionEventHandler(ExhaleNow);
+
+    }
+
+    private void InhaleNow(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("X clicked");
+    }
+
+    private void ExhaleNow(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Y clicked");
+    }
+}
